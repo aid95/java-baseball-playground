aefdd4a9a53a19dcf1ef7a50972c8899c1af33f7
Byeongju, Shin <cd80@kakao.com>
2021-12-17 12:45:23 +0900

    test: 공 생성 유효범위 검사 케이스 추가

--- src/test/java/study/baseball/domain/BallTest.java
+++ src/test/java/study/baseball/domain/BallTest.java
@@ -4,11 +4,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class BallTest {
 
   @Test
-  void testCompareTwoSlot() {
+  void 두_공을_비교한다() {
     Ball ball1 = new Ball(1);
     Ball ball2 = new Ball(1);
 
@@ -17,10 +19,9 @@ public class BallTest {
     assertThat(result).isTrue();
   }
 
-  @Test
-  void testInvalidValueSlot() {
-    assertThatThrownBy(() -> {
-      new Ball(10);
-    }).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("1~9");
+  @ParameterizedTest
+  @ValueSource(ints = {0, 10})
+  void 공은_1_부터_9_까지의_수를_가진다(int value) {
+    assertThatThrownBy(() -> new Ball(value)).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("1~9");
   }
 }
